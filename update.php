760aa33e2af8069fdff31ed829a905a8319f39b2
MinHo <37391448+hohaiminh@users.noreply.github.com>
2020-08-27 17:45:00 +0700

    Add files via upload

---
+++ update.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cập nhật dữ liệu MySQL với PHP</title>
+</head>
+<body>
+    <?php
+    // Truy vấn database để lấy danh sách
+    // 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
+    // C:\xampp\htdocs\web02\
+    include_once(__DIR__ . '/connect.php');
+    // 2. Chuẩn bị QUERY
+    // HERE DOC
+    $httt_ma = 1;
+    $httt_ten = 'chuyển khoản ngân hàng updated';
+    $sql = <<<EOT
+    UPDATE hinhthucthanhtoan
+    SET
+        httt_ten='$httt_ten'
+    WHERE httt_ma=$httt_ma
+EOT;
+    // 3. Yêu cầu PHP thực thi QUERY
+    mysqli_query($conn, $sql);
+    ?>
+</body>
+</html>

4f4817eafb9ea3596b6abf74928b6d38a3a56d68
hohaiminhh@gmail.com <hohaiminhh@gmail.com>
2020-09-04 23:03:20 +0700

    Thay doi

--- Minh/backend/layouts/scripts.php
+++ Minh/backend/layouts/scripts.php
@@ -1,9 +1,11 @@
-<script src="/Minh/assets/vendor/jquery/jquery.min.js"></script>
-<script src="/Minh/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="/Minh/assets/vendor/bootstrap/css/bootstrap.min.css"></script>
+<script src="/../../assets/vendor/jquery/jquery.min.js"></script>
+<script src="/../../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="/../../assets/vendor/bootstrap/css/bootstrap.min.css"></script>
 
 
 
 <!-- Thư viện Jquery validation -->
-<script src="/Minh/assets/vendor/jquery-validation/jquery.validate.min.js"></script>
-<script src="/Minh/assets/vendor/jquery-validation/localization/messages_vi.min.js"></script>
+<script src="/../../assets/vendor/jquery-validation/jquery.validate.min.js"></script>
+<script src="/../../assets/vendor/jquery-validation/localization/messages_vi.min.js"></script>
+
+

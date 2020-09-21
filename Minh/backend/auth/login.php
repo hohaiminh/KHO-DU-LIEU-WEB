ada7c3a0ccbb691e032257d0de9e5959046a94d7
hohaiminhh@gmail.com <hohaiminhh@gmail.com>
2020-09-21 23:10:17 +0700

    HOAN TAT

--- Minh/backend/auth/login.php
+++ Minh/backend/auth/login.php
@@ -1,33 +1,45 @@
+<?php
+// hàm `session_id()` sẽ trả về giá trị SESSION_ID (tên file session do Web Server tự động tạo)
+// - Nếu trả về Rỗng hoặc NULL => chưa có file Session tồn tại
+if (session_id() === '') {
+    // Yêu cầu Web Server tạo file Session để lưu trữ giá trị tương ứng với CLIENT (Web Browser đang gởi Request)
+    session_start();
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CREATE</title>
+    <title>Đăng nhập Backend | NenTang.vn</title>
 
-        <?php include_once(__DIR__ . '/../layouts/styles.php'); ?>
+    <!-- Nhúng file Quản lý các Liên kết CSS dùng chung cho toàn bộ trang web -->
+    <?php include_once(__DIR__ . '/../layouts/styles.php'); ?>
 </head>
 
-<body>
+<body class="d-flex flex-column h-100">
     <!-- header -->
-
     <?php include_once(__DIR__ . '/../layouts/partials/header.php'); ?>
+    <!-- end header -->
 
-    
     <div class="container">
         <div class="row">
+
             <main role="main" class="col-md-12 ml-sm-auto px-4 mb-2">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2">Đăng nhập Backend</h1>
                 </div>
+
                 <!-- Block content -->
                 <?php
                 // Đã đăng nhập rồi -> điều hướng về trang chủ
                 if (isset($_SESSION['kh_tendangnhap_logged']) && !empty($_SESSION['kh_tendangnhap_logged'])) :
                 ?>
-                    <h2>Bạn đã đăng nhập rồi. <a href="/Minh/backend/">Bấm vào đây để quay về trang chủ.</a></h2>
+                    <h2>Bạn đã đăng nhập rồi. <a href="/KHO-DU-LIEU-WEB/Minh/backend/pages/dashboard.php">Bấm vào đây để quay về trang chủ.</a></h2>
                 <?php else : ?>
+
                     <form name="frmLogin" id="frmLogin" method="post" action="">
                         <div class="row justify-content-center">
                             <div class="col-md-8">
@@ -81,6 +93,7 @@
                             </div>
                         </div>
                     </form>
+
                 <?php
                     // Hiển thị tất cả lỗi trong PHP
                     // Chỉ nên hiển thị lỗi khi đang trong môi trường Phát triển (Development)
@@ -88,14 +101,17 @@
                     ini_set('display_errors', 1);
                     ini_set('display_startup_errors', 1);
                     error_reporting(E_ALL);
+
                     // Truy vấn database
                     // 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
                     include_once(__DIR__ . '/../../connect.php');
+
                     // Chưa đăng nhập -> Xử lý logic/nghiệp vụ kiểm tra Tài khoản và Mật khẩu trong database
                     if (isset($_POST['btnLogin'])) {
                         // Phân tách thông tin từ người dùng gởi đến qua Request POST
                         $kh_tendangnhap = $_POST['kh_tendangnhap'];
                         $kh_matkhau = $_POST['kh_matkhau'];
+
                         // Câu lệnh SELECT Kiểm tra đăng nhập...
                         $sqlSelect = <<<EOT
                         SELECT *
@@ -104,11 +120,14 @@
 EOT;
                         // Thực thi SELECT
                         $result = mysqli_query($conn, $sqlSelect);
+
                         // Sử dụng hàm `mysqli_num_rows()` để đếm số dòng SELECT được
                         // Nếu có bất kỳ dòng dữ liệu nào SELECT được <-> Người dùng có tồn tại và đã đúng thông tin USERNAME, PASSWORD
                         if (mysqli_num_rows($result) > 0) {
+
                             // Lưu thông tin Tên tài khoản user đã đăng nhập
                             $_SESSION['kh_tendangnhap_logged'] = $kh_tendangnhap;
+
                             echo 'Đăng nhập thành công!';
                             // Điều hướng (redirect) về trang chủ
                             echo '<script>location.href = "/KHO-DU-LIEU-WEB/Minh/backend/pages/dashboard.php";</script>';
@@ -122,13 +141,16 @@ EOT;
             </main>
         </div>
     </div>
-        <!-- footer -->
 
+    <!-- footer -->
     <?php include_once(__DIR__ . '/../layouts/partials/footer.php'); ?>
+    <!-- end footer -->
 
+    <!-- Nhúng file quản lý phần SCRIPT JAVASCRIPT -->
+    <?php include_once(__DIR__ . '/../layouts/scripts.php'); ?>
 
-
-<?php include_once(__DIR__ . '/../layouts/scripts.php'); ?>
-
+    <!-- Các file Javascript sử dụng riêng cho trang này, liên kết tại đây -->
+    <!-- <script src="..."></script> -->
 </body>
+
 </html>
--- Minh/backend/layouts/partials/header.php
+++ Minh/backend/layouts/partials/header.php
@@ -3,13 +3,16 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="/KHO-DU-LIEU-WEB/Minh/backend/pages/dashboard.php">Bảng tin</a>
       </li>
     </ul>
+
     <ul class="navbar-nav px-3 ml-auto">
+
       <?php
       // Đã đăng nhập rồi -> hiển thị tên Người dùng và menu Đăng xuất
       if (isset($_SESSION['kh_tendangnhap_logged']) && !empty($_SESSION['kh_tendangnhap_logged'])) :
